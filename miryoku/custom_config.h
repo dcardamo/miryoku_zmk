3698c138b7521f452a39542a999835b3c88e3421
Dan Cardamore <dcardamo@users.noreply.github.com>
2023-06-20 22:12:00 -0400

    take out inverted T

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,7 +6,6 @@
 #define MIRYOKU_EXTRA_QWERTY
 #define MIRYOKU_NAV_VI
 #define MIRYOKU_CLIPBOARD_MAC
-#define MIRYOKU_NAV_INVERTEDT
 
 
 // https://github.com/LilRooks/miryoku_zmk/blob/master/miryoku/custom_config.h
